10f8824a04e0455769d7fed762ad7332bcc76c50
DoKTopBaHTyc <denisoid93@gmail.com>
2025-03-12 18:33:15 +0300

    db

--- server/db/migrations/20250312142554-create-word.js
+++ server/db/migrations/20250312142554-create-word.js
@@ -2,49 +2,36 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Events', {
+    await queryInterface.createTable('Words', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-        
+        type: Sequelize.INTEGER,
       },
-      eventDate: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      title: {
+      name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      text: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      address: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      maxParticipants: {
+      categoryId: {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
-      countParticipants: {
+      userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Events');
-  }
+    await queryInterface.dropTable('Words');
+  },
 };
