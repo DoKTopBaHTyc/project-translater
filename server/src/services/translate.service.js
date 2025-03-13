7f28fb95fff133f28095ace6ca90a8d85c475bbf
37Lizz <elizavetapozdniakova37@gmail.com>
2025-03-13 12:08:55 +0300

    Done

--- server/src/services/translate.service.js
+++ server/src/services/translate.service.js
@@ -1,36 +1,33 @@
-const { Translation } = require('../../db/models');
+const { Translation, Word } = require('../../db/models');
 
 class TranslateService {
-  static async getAllLangEn({ languageId }) {
-    const words = await Translation.findAll({
-      where: { languageId },
-      attributes: ['name', 'en'],
+  static async getAllLangWord(languageId) {
+    const words = await Word.findAll({
+      attributes: ['name'],
+      include: [
+        {
+          model: Translation,
+          attributes: ['translation'],
+          where: { languageId },
+        },
+      ],
     });
-    return words;
+
+    const result = words.map((word,index) => ({
+      name: word.name,
+      translation: word.Translations[index].translation,
+    }));
+    return result;
   }
 
-  static async getAllLangFr({ languageId }) {
-    const words = await Translation.findAll({
-      where: { languageId },
-    });
-    return words;
-  }
-
-  static async getAllLangJa({ languageId }) {
-    const words = await Translation.findAll({
-      where: { languageId },
-    });
-    return words;
-  }
-
-  static async createTranslation({ userId, languageIdId, translation }) {
-    const word = await Translation.create({ userId, languageIdId, translation });
+  static async createTranslation({ wordId, languageId, translation }) {
+    const word = await Translation.create({ wordId, languageId, translation });
     return word;
   }
 
   static async updateTranslation({ translation, id }) {
     const word = await Translation.findByPk(id);
-    const updateword = await word.update({translation});
+    const updateword = await word.update({ translation });
     return updateword;
   }
 
@@ -38,7 +35,6 @@ class TranslateService {
     const deletedRows = await Translation.destroy({ where: { id } });
     return deletedRows;
   }
-
 
   static async checkTranslit({ translation, id }) {
     const word = await Translation.findByPk(id);
