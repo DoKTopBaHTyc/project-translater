931abbf3900bee3af3bf9092b2c4c2af517bf92e
Your Name <you@example.com>
2025-03-14 18:21:03 +0300

    deploy

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
+const path = require('path')
 const authRouter = require('./routes/authRouter');
 const tokensRouter = require('./routes/tokensRouter');
 const eventRouter = require('./routes/eventRouter');
@@ -16,5 +17,9 @@ app.use('/api/auth', authRouter);
 app.use('/api/tokens', tokensRouter);
 app.use('/api', eventRouter);
 app.use('/api', translitRouter);
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
 
 module.exports = app;
