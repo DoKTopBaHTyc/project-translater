7a9ae1b535d788edfbcd0783c68c47f2bc2e4126
DoKTopBaHTyc <denisoid93@gmail.com>
2025-03-14 17:47:46 +0300

    done

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const express = require('express');
+const path = require('path');
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}...`));
